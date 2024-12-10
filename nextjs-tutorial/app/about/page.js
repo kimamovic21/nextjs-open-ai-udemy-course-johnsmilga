f272cc13c3c67389de9d6b3fdd0ddce9addffa97
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-10 13:23:23 +0100

    10. Link Component

--- nextjs-tutorial/app/about/page.js
+++ nextjs-tutorial/app/about/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 const AboutPage = () => {
   return (
     <div>
       <h2 className='text-7xl'>
         About Page
       </h2>
+      <Link href='/' className='text-2xl'>
+        home page
+      </Link>
     </div>
   );
 };
--- nextjs-tutorial/app/page.js
+++ nextjs-tutorial/app/page.js
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>
         Home Page
       </h1>
+      <Link href='/about' className='text-2xl'>
+        about page
+      </Link>
     </div>
   );
 };
