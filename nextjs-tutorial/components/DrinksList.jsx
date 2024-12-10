08cbc949695ac1ba239d05cb8f8fbf59097ce087
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-10 16:47:31 +0100

    26. Single Drink - Challenge

--- nextjs-tutorial/components/DrinksList.jsx
+++ nextjs-tutorial/components/DrinksList.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const DrinksList = ({ drinks }) => {
   return (
     <ul className='menu menu-vertical pl-0'>
-      {drinks.map((drink) => (
+      {drinks?.map((drink) => (
         <li key={drink.idDrink}>
           <Link
             href={`/drinks/${drink.idDrink}`}
