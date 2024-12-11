595acc0f04dce9aad75b68662759cb6a83fd750d
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-11 16:49:50 +0100

    33. Prisma CRUD

--- nextjs-tutorial/app/prisma-example/page.js
+++ nextjs-tutorial/app/prisma-example/page.js
@@ -1,9 +1,35 @@
-const PrismaExamplePage = () => {
+import prisma from '@/utils/db';
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: 'wake up',
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return allTasks;
+};
+
+const PrismaExamplePage = async () => {
+  const tasks = await prismaHandlers();
+
   return (
     <div>
-      <h2 className='text-7xl'>
+      <h2 className='text-3xl font-medium mb-4'>
         Prisma Example Page
       </h2>
+      {tasks?.map((task) => {
+        return (
+          <h2 key={task.id} className='text-xl py-2'>
+            😬 {task.content}
+          </h2>
+        );
+      })}
     </div>
   );
 };
--- nextjs-tutorial/components/Navbar.jsx
+++ nextjs-tutorial/components/Navbar.jsx
@@ -5,6 +5,7 @@ const links = [
   { href: '/drinks', label: 'drinks' },
   { href: '/tasks', label: 'tasks' },
   { href: '/query', label: 'react-query' },
+  { href: '/prisma-example', label: 'prisma' },
 ];
 
 const Navbar = () => {
