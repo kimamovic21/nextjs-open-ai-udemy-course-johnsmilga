21139a8dcb056e6a55e826bf6834ae856d4a25e1
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-11 17:22:40 +0100

    36. First Server Action

--- nextjs-tutorial/components/TaskForm.jsx
+++ nextjs-tutorial/components/TaskForm.jsx
@@ -1,6 +1,36 @@
-const TaskForm = () => {
+import { revalidatePath } from 'next/cache';
+import prisma from '@/utils/db';
+
+const createTask = async (formData) => {
+  'use server';
+  const content = formData.get('content');
+  console.log(content);
+
+  await prisma.task.create({
+    data: {
+      content,
+    },
+  });
+
+  revalidatePath('/tasks');
+};
+
+const TaskForm = async () => {
   return (
-    <div>TaskForm</div>
+    <form action={createTask}>
+      <div className='join w-full'>
+        <input
+          className='input input-bordered join-item w-full'
+          placeholder='Type Here'
+          type='text'
+          name='content'
+          required
+        />
+        <button type='submit' className='btn join-item btn-primary'>
+          create task
+        </button>
+      </div>
+    </form>
   );
 };
 
