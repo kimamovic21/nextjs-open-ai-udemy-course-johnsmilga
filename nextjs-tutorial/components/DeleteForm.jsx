4fa200c955f3833cc658206b12aa42ecdadc5e72
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-14 12:51:57 +0100

    45. Delete Button - Challenge

--- nextjs-tutorial/components/DeleteForm.jsx
+++ nextjs-tutorial/components/DeleteForm.jsx
@@ -1,12 +1,23 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
 import { deleteTask } from '@/utils/actions';
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  
+  return (
+    <button type='submit' className='btn btn-xs btn-error' disabled={pending}>
+      {pending ? 'pending...' : 'delete'}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-error btn-xs'>
-        delete
-      </button>
+      <SubmitButton />
     </form>
   );
 };
