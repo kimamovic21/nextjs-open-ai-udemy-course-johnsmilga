fb4b0438e98273d29b8d2dfb9d8c5eb12f368ce3
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-19 14:59:45 +0100

    83. New Tour - Form

--- gptgenius/components/NewTour.jsx
+++ gptgenius/components/NewTour.jsx
@@ -1,6 +1,47 @@
+'use client';
+
+import TourInfo from '@/components/TourInfo';
+
 const NewTour = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    console.log('formData: ', formData);
+    const destination = Object.fromEntries(formData.entries());
+    console.log('destination: ', destination);
+  };
+
   return (
-    <div>NewTour</div>
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className=' mb-4'>
+          Select your dream destination
+        </h2>
+        <div className='join w-full'>
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='city'
+            name='city'
+            required
+          />
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='country'
+            name='country'
+            required
+          />
+          <button className='btn btn-primary join-item' type='submit'>
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className='mt-16'>
+        <TourInfo />
+      </div>
+    </>
   );
 };
 
