c995939db055e033840ece277ddea159fa1d8efb
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-23 16:56:12 +0100

    97. Generate Image

--- gptgenius/app/(dashboard)/tours/[id]/page.js
+++ gptgenius/app/(dashboard)/tours/[id]/page.js
@@ -1,6 +1,7 @@
 import { redirect } from 'next/navigation';
-import { getSingleTour } from '@/utils/actions';
+import { generateTourImage, getSingleTour } from '@/utils/actions';
 import Link from 'next/link';
+import Image from 'next/image';
 import TourInfo from '@/components/TourInfo';
 
 const SingleTourPage = async ({ params }) => {
@@ -9,11 +10,30 @@ const SingleTourPage = async ({ params }) => {
     redirect('/tours');
   };
 
+  const tourImage = await generateTourImage({
+    city: tour.city,
+    country: tour.country,
+  });
+
   return (
     <div>
       <Link href='/tours' className='btn btn-secondary mb-12'>
         back to tours
       </Link>
+
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            width={300}
+            height={300}
+            className='rounded-xl shadow-xl mb-16 h-96 w-96 object-cover'
+            alt={tour.title}
+            priority
+          />
+        </div>
+      ) : null}
+
       <TourInfo tour={tour} />
     </div>
   );
