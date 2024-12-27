d393d213673af547e1e045cd1676b8712ce4bd12
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-27 14:26:10 +0100

    111. Shadcn-ui

--- jobify/app/page.tsx
+++ jobify/app/page.tsx
@@ -1,4 +1,13 @@
-const HomePage = () => {
-  return <div>Jobiby Starter project</div>;
+import { Camera } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <Button>default button</Button>
+      <Button variant='outline' size='icon'>
+        <Camera />
+      </Button>
+    </div>
+  );
 };
-export default HomePage;
