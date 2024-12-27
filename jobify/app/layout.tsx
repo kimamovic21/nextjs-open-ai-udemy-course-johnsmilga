d5e8472d5491da42f35c1744bef2cc2a62c31f0a
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-27 15:02:58 +0100

    113. Layout and Homepage

--- jobify/app/layout.tsx
+++ jobify/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,4 +19,4 @@ export default function RootLayout({
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+};
--- jobify/app/page.tsx
+++ jobify/app/page.tsx
@@ -1,13 +1,38 @@
-import { Camera } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../assets/logo.svg';
+import LandingImg from '../assets/main.svg';
 
 export default function Home() {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      <Button>default button</Button>
-      <Button variant='outline' size='icon'>
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
+        <Image src={Logo} alt='logo' />
+      </header>
+      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
+        <div>
+          <h1 className='capitalize text-4xl md:text-7xl font-bold'>
+            <span className='mr-2'>job</span>
+            <span className='text-primary'>tracking</span> 
+            <span className='ml-2'>App</span>
+          </h1>
+          <p className='leading-loose max-w-md mt-4 '>
+            The Jobify project is a full-stack job tracking application built using the Next.js Framework.
+            It allows users to manage job applications efficiently by providing features such as job creation, 
+            updating, and deletion. Users can track their job position, job status, job type, job location and 
+            company name offering an organized way to manage their job hunt.
+          </p>
+          <Button asChild className='mt-4'>
+            <Link href='/add-job'>Get Started</Link>
+          </Button>
+        </div>
+        <Image 
+          src={LandingImg} 
+          alt='landing' 
+          className='hidden lg:block ' 
+        />
+      </section>
+    </main>
   );
 };
