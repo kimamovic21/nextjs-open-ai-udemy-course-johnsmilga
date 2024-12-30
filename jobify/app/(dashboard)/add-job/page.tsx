c85b1442760640a45b2fa5d826a9c5304a5149c1
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-30 15:54:23 +0100

    131. React Query - Setup

--- jobify/app/(dashboard)/add-job/page.tsx
+++ jobify/app/(dashboard)/add-job/page.tsx
@@ -1,10 +1,17 @@
-import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import CreateJobForm from '@/components/CreateJobForm';
 
 function AddJobPage() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobForm />
-    </>
+    </HydrationBoundary>
   );
 };
 
