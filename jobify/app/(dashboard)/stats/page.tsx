eb3766944916ea394a416f565f291eb44da2d540
kimamovic21 <imamovic.kerim@gmail.com>
2025-01-03 17:03:23 +0100

    147. Charts Action

--- jobify/app/(dashboard)/stats/page.tsx
+++ jobify/app/(dashboard)/stats/page.tsx
@@ -1,8 +1,10 @@
-import { getStatsAction } from '@/utils/actions';
+import { getStatsAction, getChartsDataAction } from '@/utils/actions';
 
 async function StatsPage() {
   const stats = await getStatsAction();
   console.log('Stats:', stats);
+  const charts = await getChartsDataAction();
+  console.log('Charts:', charts);
 
   return (
     <div>StatsPage</div>
